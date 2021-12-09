a6be48215ca5d4d4f3ea5730736e72edd23b5247
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-09 08:53:37 +0000

    bug fixed

--- functions/convertTrApiData.js
+++ functions/convertTrApiData.js
@@ -1,16 +1,19 @@
 const convertTrApiData = (data) => {
   if (data.transaction_list.length == 0) {
-    return {};
+    return [false, {}];
   } else {
     const recent = data.transaction_list.slice(-1)[0];
     // console.log(recent.NRG_DL_AM);
     const price = parseInt(recent.NRG_DL_AM.replace(/,/g, "")) * 10000;
     const price_per_area = price / parseFloat(recent.NRG_AR);
-    return {
-      price: price,
-      price_per_area: price_per_area,
-      date: recent.NRG_DL_M.slice(0, 4) + "." + recent.NRG_DL_M.slice(4),
-    };
+    return [
+      true,
+      {
+        price: price,
+        price_per_area: price_per_area,
+        date: recent.NRG_DL_M.slice(0, 4) + "." + recent.NRG_DL_M.slice(4),
+      },
+    ];
   }
 };
 
