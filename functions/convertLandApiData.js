ac1eb0f45e3d370cefed4b0500ded1052d3b9999
ChangyongKim0 <changyong.kim.0@gmail.com>
2021-12-09 08:53:21 +0000

    bug fixed

--- functions/convertLandApiData.js
+++ functions/convertLandApiData.js
@@ -1,4 +1,4 @@
-const convertBldgApiData = (data) => {
+const convertLandApiData = (data) => {
   let usage_list = [];
   data.LNSD_PRP_DSTRC_NM
     ? data.LNSD_PRP_DSTRC_NM.split(",").map((e) => {
@@ -7,21 +7,24 @@ const convertBldgApiData = (data) => {
     : {};
   const new_usage_list = Array.from(new Set(usage_list));
 
-  return {
-    area: parseFloat(data.LND_AR),
-    usage: data.LND_USE_NM,
-    base_ymd_list: [0, 1, 2, 3, 4].map(
-      (e) => String(parseInt(data.LNPD_YR) - e) + "." + data.LNPD_MT
-    ),
-    price_list: [
-      parseFloat(data.LNPD_PY0),
-      parseFloat(data.LNPD_PY1),
-      parseFloat(data.LNPD_PY2),
-      parseFloat(data.LNPD_PY3),
-      parseFloat(data.LNPD_PY4),
-    ],
-    usage_list: new_usage_list,
-  };
+  return [
+    true,
+    {
+      area: parseFloat(data.LND_AR),
+      usage: data.LND_USE_NM,
+      base_ymd_list: [0, 1, 2, 3, 4].map(
+        (e) => String(parseInt(data.LNPD_YR) - e) + "." + data.LNPD_MT
+      ),
+      price_list: [
+        parseFloat(data.LNPD_PY0),
+        parseFloat(data.LNPD_PY1),
+        parseFloat(data.LNPD_PY2),
+        parseFloat(data.LNPD_PY3),
+        parseFloat(data.LNPD_PY4),
+      ],
+      usage_list: new_usage_list,
+    },
+  ];
 };
 
-module.exports = convertBldgApiData;
+module.exports = convertLandApiData;
