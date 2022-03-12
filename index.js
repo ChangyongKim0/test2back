74f694bd527b5a9a4409f27532f4cad8ac07ffa9
ChangyongKim0 <cyklone001@gmail.com>
2022-03-12 19:28:55 +0000

    CORS bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var bodyParser = require("body-parser");
 var express = require("express");
 var mysql = require("mysql");
+var cors = require("cors");
 const handleCookieData = require("./functions/handleCookieData");
 const getBldgInfoData = require("./functions/getBldgInfoData");
 const getMapInfoData = require("./functions/getMapInfoData");
@@ -14,10 +15,29 @@ const mysql_option = {
 };
 
 var app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.opts
+
+// app.use(cors({
+//     origins: ['https://api.propi.moohae.net', 'https://propi.moohae.net'],   // defaults to ['*']
+//     credentials: true,                 // defaults to false
+//     // headers: ['x-foo']                 // sets expose-headers
+// }));
+
+app.use(cors({
+    origins: ['https://propi.moohae.net']
+}));
+
 const port = 3001;
 app.set("port", port);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get("/", (req, res)=> {
+  res.send("server testing ok");
+  console.log("server testing ok");
+});
 
 app.get("/api", function (req, res) {
   console.log("Hello World!");
